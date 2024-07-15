e07cb6ff4e54a2a95f8291a7f7916272be63284c
Sooyeon Yang <soois1114@gmail.com>
2024-07-14 19:54:27 -0500

    asdfasdg

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 @export var PLAYER_VELOCITY = 10
+@export var game_stats: GameStats
 
-var score = 0
+@onready var score_label: Label = $Score
+
+var Score = 0
 var highscore = 0 # setting highscore to 0 here is a bad idea (have to look up how to keep track of it - maybe use global??)
 var current_item = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	reset()
+	$MeatPlayer.position = Vector2(1400, 350)
+	$VegPlayer.position = Vector2(510, 350)
+	update_score_label(game_stats.score)
+	game_stats.score_changed.connect(update_score_label)
+	# might be nice to put a freeze here and then say the instructions before starting (add this last since we might not have enough time)
 	# add_player_interactives()
 
 
@@ -33,12 +40,6 @@ func _input(event):
 		$VegPlayer.active = $MeatPlayer.active
 		$MeatPlayer.active = temp_state
 			
-func reset():
-	$MeatPlayer.position = Vector2(1400, 350)
-	$VegPlayer.position = Vector2(510, 350)
-	
-	# might be nice to put a freeze here and then say the instructions before starting (add this last since we might not have enough time)
-
 # add a function that checks if life or mad customer count has reached game end amount and act accordingly
 # add functions for player interactions with items
 
@@ -51,3 +52,5 @@ func unfreeze():
 	# unfreeze for the same as above
 	pass
 	
+func update_score_label(new_score: int) -> void:
+	score_label.text = "Score: " + str(new_score)
