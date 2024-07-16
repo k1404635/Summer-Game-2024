574e895eded10961decbcfd83c981b7d62a9e07b
Sooyeon Yang <soois1114@gmail.com>
2024-07-16 00:59:30 -0500

    collisions refixed

---
+++ Scripts/npc2_going_in_path.gd
@@ -0,0 +1,10 @@
+extends PathFollow2D
+
+@export var moveSpeed = 60
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	set_progress(get_progress() + moveSpeed * delta)
+	$AnimatedSprite2D.play("NPC2Back")
+	if (get_progress_ratio() == 1):
+		$AnimatedSprite2D.play("NPC2BackStill")
