86f3de1307cc6ffd772bfe7c05181f0341f51b51
Sooyeon Yang <soois1114@gmail.com>
2024-07-26 19:09:18 -0500

    added lives (still wip)

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -8,4 +8,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (Global.lives == 3):
+		$Life1.visible = true
+		$Life2.visible = true
+		$Life3.visible = true
+	elif (Global.lives == 2):
+		$Life1.visible = true
+		$Life2.visible = true
+		$Life3.visible = false
+	elif (Global.lives == 1):
+		$Life1.visible = true
+		$Life2.visible = false
+		$Life3.visible = false
+	elif (Global.lives == 0): # it shouldn't ever need to do this since it should just go to game over screen, but putting this here just in case
+		$Life1.visible = false
+		$Life2.visible = false
+		$Life3.visible = false
