6f4ab84186c724b0dfa8da68108ea8cf0c35f9bc
Sooyeon Yang <soois1114@gmail.com>
2024-08-06 15:11:28 -0500

    npc path rougly finished
    just need to add showing the order bubble, patience bar, and then if they get the order/dont get it in time, move again and leave.

--- Scripts/npc1_going_in_path.gd
+++ Scripts/npc1_going_in_path.gd
@@ -1,10 +1,20 @@
 extends PathFollow2D
 
 @export var moveSpeed = 60
+@onready var halfway = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	set_progress(get_progress() + moveSpeed * delta)
-	$AnimatedSprite2D.play("NPC1Front")
-	if (get_progress_ratio() == 1):
+	if(get_progress_ratio() > 0.5 and halfway == false):
+		moveSpeed = 0
+		$AnimatedSprite2D.play("NPC1BackStill")
+	else:
+		moveSpeed = 60
+	if (get_progress_ratio() < 0.5):
+		$AnimatedSprite2D.play("NPC1Back")
+	elif (get_progress_ratio() > 0.5 and get_progress_ratio() < 1 and halfway != false):
+		$AnimatedSprite2D.play("NPC1Front")
+	elif (get_progress_ratio() == 1): # need to add another if statement or something that makes it move again (i.e. moveSpeed = 60 again)
 		$AnimatedSprite2D.play("NPC1FrontStill")
+		
+	set_progress(get_progress() + moveSpeed * delta)
--- Scripts/npc2_going_in_path.gd
+++ Scripts/npc2_going_in_path.gd
@@ -1,10 +1,20 @@
 extends PathFollow2D
 
 @export var moveSpeed = 60
+@onready var halfway = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	set_progress(get_progress() + moveSpeed * delta)
-	$AnimatedSprite2D.play("NPC2Front")
-	if (get_progress_ratio() == 1):
+	if(get_progress_ratio() > 0.5 and halfway == false):
+		moveSpeed = 0
+		$AnimatedSprite2D.play("NPC2BackStill")
+	else:
+		moveSpeed = 60
+	if (get_progress_ratio() < 0.5):
+		$AnimatedSprite2D.play("NPC2Back")
+	elif (get_progress_ratio() > 0.5 and get_progress_ratio() < 1 and halfway != false):
+		$AnimatedSprite2D.play("NPC2Front")
+	elif (get_progress_ratio() == 1): # need to add another if statement or something that makes it move again (i.e. moveSpeed = 60 again)
 		$AnimatedSprite2D.play("NPC2FrontStill")
+		
+	set_progress(get_progress() + moveSpeed * delta)
