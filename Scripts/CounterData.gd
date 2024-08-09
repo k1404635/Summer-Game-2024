2b31a23c522777e51e8ad6f37d6982cb179e1654
k1404635 <soois1114@gmail.com>
2024-08-08 20:13:14 -0500

    interactions with environment including dropping items almost done

---
+++ Scripts/CounterData.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name CounterData
+
+@export var CounterName : String
+# @export var Icon : Texture2D
+
+@export var CounterModelPrefab : PackedScene
--- Scripts/PlayerInteractionHandler.gd
+++ Scripts/PlayerInteractionHandler.gd
@@ -4,16 +4,18 @@ signal OnItemPickedUp(item)
 signal OnItemDropped(item)
 
 @export var ItemTypes : Array[ItemData] = []
+@export var CounterTypes : Array[CounterData] = []
 
+var NearbyCounters : Array[InteractableCounters]
 var NearbyBodies : Array[InteractableItem]
-@export var itemsInInventory = 0
+var InventoryItems : Array[InteractableItem]
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("Pickup or Put down")):
-		if itemsInInventory == 1:
-			PutDown()
-		else:
+		if InventoryItems.is_empty():
 			Pickup()
+		else:
+			PutDown()
 		
 func Pickup():
 	var nearestItem : InteractableItem = null
@@ -22,51 +24,57 @@ func Pickup():
 		if(item.global_position.distance_to(global_position) < nearestDistance):
 			nearestDistance = item.global_position.distance_to(global_position)
 			nearestItem = item
+			
 		
-		if nearestItem != null and itemsInInventory == 0:
-			nearestItem.queue_free()
-			NearbyBodies.remove_at(NearbyBodies.find(nearestItem))
+		if nearestItem != null and InventoryItems.is_empty():
+			if(!(nearestItem.name == "EggplantSack" or nearestItem.name == "CabbageSack" or nearestItem.name == "MeatSack" or nearestItem.name == "TomatoSack")):
+				nearestItem.queue_free()
+				NearbyBodies.remove_at(NearbyBodies.find(nearestItem))
 			var itemPrefab = nearestItem.scene_file_path
 			for i in ItemTypes.size():
 				if(ItemTypes[i].ItemModelPrefab != null and ItemTypes[i].ItemModelPrefab.resource_path == itemPrefab):
 					print("Item id:" + str(i) + " Item Name: " + ItemTypes[i].ItemName)
-					itemsInInventory += 1
+					InventoryItems.append(nearestItem)
 					OnItemPickedUp.emit(ItemTypes[i])
 					return
 				
-				printerr("Item not found") 
+			printerr("Item not found") 
 
 func PutDown(): # FIX THIS METHOD AFTER FIGURING OUT HOW TO DO THE TABLE INTERACTION INSTEAD OF INDIVIDUAL ITEMS
-	if(!NearbyBodies.is_empty()): # need to put everything below in this if statement probs??
+	if(!NearbyCounters.is_empty()): # need to put everything below in this if statement probs??
 		pass
 		
 	#change the item parts to fit the tables instead of items
-	var nearestItem : InteractableItem = null
+	var nearestCounter : InteractableCounters = null
 	var nearestDistance : float = INF
-	for item in NearbyBodies:
-		if(item.global_position.distance_to(global_position) < nearestDistance):
-			nearestDistance = item.global_position.distance_to(global_position)
-			nearestItem = item
+	for counters in NearbyCounters:
+		if(counters.global_position.distance_to(global_position) < nearestDistance):
+			nearestDistance = counters.global_position.distance_to(global_position)
+			nearestCounter = counters
 		
-		if nearestItem != null and itemsInInventory == 1:
-			nearestItem.queue_free() #instead of queue_free, create the corresponding food image on the table
-			NearbyBodies.remove_at(NearbyBodies.find(nearestItem))
-			var itemPrefab = nearestItem.scene_file_path #this would need to change based on what's currently in the inventory and not based on the nearestItem
-			for i in ItemTypes.size(): # i could probs keep this as itemTypes, but need to make a table/tile types or something that is similar to itemtypes for the rest of this
+		var ItemName = null
+		if nearestCounter != null and InventoryItems.size() == 1:
+			nearestCounter.add_child(InventoryItems[0])
+			var itemPrefab = InventoryItems[0].scene_file_path
+			for i in ItemTypes.size(): 
 				if(ItemTypes[i].ItemModelPrefab != null and ItemTypes[i].ItemModelPrefab.resource_path == itemPrefab):
-					print("Item id:" + str(i) + " Item Name: " + ItemTypes[i].ItemName)
-					itemsInInventory += 1
-					OnItemDropped.emit(ItemTypes[i])
-					return
-				
-				printerr("Item not found") 
+					ItemName = ItemTypes[i].ItemName
+					break
+			
+			# put all the combinations of counter types and available items to place them on that type
+			if(ItemName != null):
+				if(ItemName != "Cooked Meat" and ItemName != "Salad" and nearestCounter.name != "Table"):
+					pass
+			
 
 func OnBodyEnteredArea(body: Node2D):
 	if(body is InteractableItem):
-		body.GainFocus()
 		NearbyBodies.append(body)
+	if(body is InteractableCounters):
+		NearbyCounters.append(body)
 
 func OnBodyExitedArea(body: Node2D):
 	if(body is InteractableItem and NearbyBodies.has(body)):
-		body.LoseFocus()
 		NearbyBodies.remove_at(NearbyBodies.find(body))
+	if(body is InteractableCounters and NearbyCounters.has(body)):
+		NearbyCounters.remove_at(NearbyCounters.find(body))
